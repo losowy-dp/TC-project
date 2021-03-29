59d53927c96039e0f0676e1055f01d0768a6f2b3
texnariO <61978148+texnariO@users.noreply.github.com>
2021-03-29 05:56:13 +0300

    activity_order

---
+++ AndroidApp/ProjectCT/app/src/main/java/com/example/projectct/activity/Order.kt
@@ -0,0 +1,12 @@
+package com.example.projectct.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectct.R
+
+class Order : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
--- AndroidApp/ProjectCT/app/src/main/java/com/example/projectct/fragment/All_order_list.kt
+++ AndroidApp/ProjectCT/app/src/main/java/com/example/projectct/fragment/All_order_list.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.*
 import androidx.annotation.RequiresApi
 import com.example.projectct.R
 
@@ -37,6 +37,33 @@ class All_order_list : Fragment() {
         button.backgroundTintList = null
         val buttonMyOrder = view.findViewById<Button>(R.id.my_order_fragment)
         buttonMyOrder.setOnClickListener(MyOrderFragment)
+        var listView = view.findViewById<ListView>(R.id.allOrderList)
+        val arrayList: ArrayList<HashMap<String, String>> = ArrayList()
+        var map: HashMap<String, String>
+        //TODO change 20 on count a order in database
+        for(i in 1 .. 20)
+        {
+            map = HashMap()
+            //change on (from " ---> " where) a cites from database and 100$ change to (renumeration + " " +  currency)
+            map.put("citys","Kraków --> Warszawa")
+            map.put("value","100$")
+            arrayList.add(map)
+        }
+        val adapter = SimpleAdapter(
+                activity,
+                arrayList,
+                android.R.layout.simple_list_item_2,
+                arrayOf("citys", "value"),
+                intArrayOf(android.R.id.text1, android.R.id.text2)
+        )
+        listView.adapter = adapter
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long)
+            {
+                //remove toast and add pass a new activity
+                Toast.makeText(activity!!, "on klick", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private val MyOrderFragment = View.OnClickListener { OrderFragment() }
