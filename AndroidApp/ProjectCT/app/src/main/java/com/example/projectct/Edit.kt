4f02984c4afb3b7b0540ef3b962da3154877d2e9
Dmytro Podeiko <45311853+losowy-dp@users.noreply.github.com>
2021-04-03 18:08:56 +0300

    new edit activity

---
+++ AndroidApp/ProjectCT/app/src/main/java/com/example/projectct/Edit.kt
@@ -0,0 +1,11 @@
+package com.example.projectct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Edit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
